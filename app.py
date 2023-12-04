06a042d0f02b4a0945d909aaf90c9ddeb9199bb0
Austen199 <138988174+Austen199@users.noreply.github.com>
2023-12-04 21:14:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import subprocess
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def process_youtube_url():
+    # Get the 'url' parameter from the query string
+    youtube_url = request.args.get('url')
+
+    if youtube_url:
+        try:
+            # Construct the command based on the provided URL
+            command = f"youtube-comment-downloader --url {youtube_url} --output {youtube_url.split('=')[-1]}.json"
+            
+            # Run the command using subprocess
+            result = subprocess.run(command, shell=True, capture_output=True, text=True)
+
+            # Check if the command was successful
+            if result.returncode == 0:
+                response = {"status": "success", "output": result.stdout}
+            else:
+                response = {"status": "error", "output": result.stderr}
+
+        except Exception as e:
+            response = {"status": "error", "message": str(e)}
+
+    else:
+        response = {"status": "error", "message": "No 'url' parameter provided in the query string."}
+
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
