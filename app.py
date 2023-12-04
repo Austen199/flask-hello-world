97d0df95e35fc9c58ad83f0af819602c86fc70d8
Austen199 <138988174+Austen199@users.noreply.github.com>
2023-12-04 22:25:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,17 +10,27 @@ def process_youtube_url():
 
     if youtube_url:
         try:
-            # Construct the modified command based on the provided URL
-            command = f"youtube-comment-downloader --url {youtube_url} --pretty --output /dev/stdout"
+            # Construct the command to download comments
+            download_command = f"youtube-comment-downloader --url {youtube_url} --pretty --output {youtube_url.split('=')[-1]}.json"
             
-            # Run the command using subprocess
-            result = subprocess.run(command, shell=True, capture_output=True, text=True)
+            # Run the download command using subprocess
+            download_result = subprocess.run(download_command, shell=True, capture_output=True, text=True)
 
-            # Check if the command was successful
-            if result.returncode == 0:
-                response = {"status": "success", "output": result.stdout}
+            # Check if the download command was successful
+            if download_result.returncode == 0:
+                # Construct the command to upload the JSON file using curl
+                upload_command = f"curl --upload-file ./{youtube_url.split('=')[-1]}.json https://transfer.sh/{youtube_url.split('=')[-1]}.json"
+
+                # Run the upload command using subprocess
+                upload_result = subprocess.run(upload_command, shell=True, capture_output=True, text=True)
+
+                # Check if the upload command was successful
+                if upload_result.returncode == 0:
+                    response = {"status": "success", "download_output": download_result.stdout, "upload_output": upload_result.stdout}
+                else:
+                    response = {"status": "error", "message": f"Upload failed: {upload_result.stderr}"}
             else:
-                response = {"status": "error", "output": result.stderr}
+                response = {"status": "error", "message": f"Download failed: {download_result.stderr}"}
 
         except Exception as e:
             response = {"status": "error", "message": str(e)}
@@ -31,5 +41,4 @@ def process_youtube_url():
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True, port=5000, host='0.0.0.0', request_timeout=300)
-
+    app.run(debug=True)
