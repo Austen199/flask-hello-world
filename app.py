5171abce2181839d231eb1b55a3a1cc8127fb171
Austen199 <138988174+Austen199@users.noreply.github.com>
2023-12-05 15:34:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,26 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 import subprocess
-import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/export/', methods=['GET'])
 def process_youtube_url():
     # Get the 'url' parameter from the query string
     youtube_url = request.args.get('url')
 
     if youtube_url:
         try:
-            # Construct the command to download comments
-            download_command = f"youtube-comment-downloader --url {youtube_url} --pretty --output /dev/stdout"
+            # Construct the modified command based on the provided URL
+            command = f"youtube-comment-downloader --url {youtube_url} --pretty --output {youtube_url.split('=')[-1]}.json"
             
-            # Run the download command using subprocess
-            download_result = subprocess.run(download_command, shell=True, capture_output=True, text=True)
+            # Run the command using subprocess
+            result = subprocess.run(command, shell=True, capture_output=True, text=True)
 
-            # Check if the download command was successful
-            if download_result.returncode == 0:
-                json_content = download_result.stdout
-                response = {"status": "success", "json_content": json_content}
+            # Check if the command was successful
+            if result.returncode == 0:
+                response = {"status": "success", "output": result.stdout}
             else:
-                response = {"status": "error", "message": f"Download failed: {download_result.stderr}"}
+                response = {"status": "error", "output": result.stderr}
 
         except Exception as e:
             response = {"status": "error", "message": str(e)}
