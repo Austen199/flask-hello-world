6c585508d61d2eb986043c4d70a4c2c2cc70aa5c
Austen199 <138988174+Austen199@users.noreply.github.com>
2023-12-05 23:03:54 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,26 +1,40 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/export/', methods=['GET'])
 def process_youtube_url():
     # Get the 'url' parameter from the query string
     youtube_url = request.args.get('url')
 
     if youtube_url:
         try:
-            # Construct the modified command based on the provided URL
-            command = f"youtube-comment-downloader --url {youtube_url} --pretty --output {youtube_url.split('=')[-1]}.json"
+            # Construct the command to download comments
+            download_command = f"youtube-comment-downloader --url {youtube_url} --pretty --output {youtube_url.split('=')[-1]}.json"
             
-            # Run the command using subprocess
-            result = subprocess.run(command, shell=True, capture_output=True, text=True)
+            # Run the download command using subprocess
+            download_result = subprocess.run(download_command, shell=True, capture_output=True, text=True)
 
-            # Check if the command was successful
-            if result.returncode == 0:
-                response = {"status": "success", "output": result.stdout}
+            # Check if the download command was successful
+            if download_result.returncode == 0:
+                # Construct the command to upload the JSON file using curl
+                upload_command = f"curl --upload-file ./{youtube_url.split('=')[-1]}.json https://transfer.sh/{youtube_url.split('=')[-1]}.json"
+
+                # Run the upload command using subprocess
+                upload_result = subprocess.run(upload_command, shell=True, capture_output=True, text=True)
+
+                # Check if the upload command was successful
+                if upload_result.returncode == 0:
+                    response = {"status": "success", "upload_output": upload_result.stdout}
+                else:
+                    response = {"status": "error", "message": f"Upload failed: {upload_result.stderr}"}
             else:
-                response = {"status": "error", "output": result.stderr}
+                response = {"status": "error", "message": f"Download failed: {download_result.stderr}"}
 
         except Exception as e:
             response = {"status": "error", "message": str(e)}
