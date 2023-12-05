adb89ed985993731640c8ed1e4ad122de58aae94
Austen199 <138988174+Austen199@users.noreply.github.com>
2023-12-05 02:49:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import subprocess
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder='static', template_folder='static')
 
 # Main path route
 @app.route('/')
