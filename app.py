b8c2eda02f87767bc55519648115fdec9ec6e757
Austen199 <138988174+Austen199@users.noreply.github.com>
2023-12-06 22:33:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,125 @@
-from flask import Flask, render_template, request, jsonify
-import subprocess
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Export Social Media Comments</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+	</head>
 
-app = Flask(__name__)
+<style>
+    .loader {
+        width: 48px;
+        height: 48px;
+        border: 5px solid;
+        border-color: #FF3D00 transparent;
+        border-radius: 50%;
+        display: inline-block;
+        box-sizing: border-box;
+        animation: rotation 1s linear infinite;
+    }
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+    @keyframes rotation {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+</style>
 
-@app.route('/export/', methods=['GET'])
-def process_youtube_url():
-    # Get the 'url' parameter from the query string
-    youtube_url = request.args.get('url')
+<body>
+    <div class="container shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+        <div class="row ">
+            <div class="col-12 d-flex justify-content-center align-items-center mt-5 mb-2">
+                <h1>Export Social Media Comments</h1>
+            </div>
+        </div>
+        <div class="row shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+            <div class="col-sm-11 col p-2">
+                <form onsubmit="exportComments(event);">
+                    <div class="mb-3">
+                      <label for="url" class="form-label">YouTube URL</label>
+                      <input type="text" class="form-control" id="url" required="true">
+                    </div>
+                    <div>
+                        <button type="submit" class="btn btn-primary m-2">Export Comments</button>
+                    </div>
+                </form>
+                <div id="response" class="mt-3"></div>
+            </div>
+        </div>
+    </div>
 
-    if youtube_url:
-        try:
-            # Construct the command to download comments
-            download_command = f"youtube-comment-downloader --url {youtube_url} --pretty --output {youtube_url.split('=')[-1]}.json"
-            
-            # Run the download command using subprocess
-            download_result = subprocess.run(download_command, shell=True, capture_output=True, text=True)
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="app.js"></script>
 
-            # Check if the download command was successful
-            if download_result.returncode == 0:
-                # Construct the command to upload the JSON file using curl
-                upload_command = f"curl --upload-file ./{youtube_url.split('=')[-1]}.json https://transfer.sh/{youtube_url.split('=')[-1]}.json"
 
-                # Run the upload command using subprocess
-                upload_result = subprocess.run(upload_command, shell=True, capture_output=True, text=True)
+<script>
+    function exportComments(event) {
+        event.preventDefault();
+        const urlInput = document.getElementById('url');
+        const exportButton = event.target.querySelector('.btn.btn-primary.m-2'); // Reference the button using its class
+        const responseDiv = document.getElementById('response');
 
-                # Check if the upload command was successful
-                if upload_result.returncode == 0:
-                    response = {"status": "success", "upload_output": upload_result.stdout}
-                else:
-                    response = {"status": "error", "message": f"Upload failed: {upload_result.stderr}"}
-            else:
-                response = {"status": "error", "message": f"Download failed: {download_result.stderr}"}
+        // Disable the export button
+        exportButton.disabled = true;
 
-        except Exception as e:
-            response = {"status": "error", "message": str(e)}
+        // Show spinner instead of input and button
+        responseDiv.innerHTML = `<span class="loader"></span>`;
 
-    else:
-        response = {"status": "error", "message": "No 'url' parameter provided in the query string."}
+        const apiUrl = `/export/?url=${encodeURIComponent(urlInput.value)}`;
 
-    return jsonify(response)
+        // You can use fetch or any other AJAX library to make the request
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                if (data.status === 'success') {
+                    // Show success message
+                    responseDiv.innerHTML = `<p>Your comments are ready to download:</p>`;
 
-if __name__ == '__main__':
-    app.run(debug=True)
+                    // Show download button below the input and button
+                    const downloadButton = document.createElement('a');
+                    downloadButton.href = data.upload_output;
+                    downloadButton.target = '_blank';
+                    downloadButton.className = 'btn btn-success m-2';
+                    downloadButton.textContent = 'Download Comments';
+                    responseDiv.appendChild(downloadButton);
+
+                    // Show export new video comments button
+                    const exportNewButton = document.createElement('button');
+                    exportNewButton.className = 'btn btn-primary m-2';
+                    exportNewButton.textContent = 'Export New Video Comments';
+                    exportNewButton.addEventListener('click', () => {
+                        // Reload the page to show the initial input and button
+                        window.location.reload();
+                    });
+                    responseDiv.appendChild(exportNewButton);
+
+                    // Hide the input and original export button
+                    urlInput.style.display = 'none';
+                    exportButton.style.display = 'none';
+                } else {
+                    // Show error message
+                    responseDiv.innerHTML = `<p>Error fetching comments. Please try again.</p>`;
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching comments:', error);
+                responseDiv.innerHTML = `<p>Error fetching comments. Please try again.</p>`;
+            })
+            .finally(() => {
+                // Re-enable the export button
+                exportButton.disabled = false;
+            });
+    }
+</script>
+
+
+
+</body>
+</html>
